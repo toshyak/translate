06103a74520f6445b72416691ac4fed66cab1643
Alexey Osheychik <osheychik@gmail.com>
2020-09-16 17:59:28 +0200

    Add arg field to pass the translated text to the workflow output

--- output.go
+++ output.go
@@ -13,12 +13,13 @@ type alfredOutput struct {
 type alfredOutputItem struct {
 	Title    string            `json:"title"`
 	Subtitle string            `json:"subtitle"`
+	Arg      string            `json:"arg"`
 	Icon     map[string]string `json:"icon"`
 }
 
 func output(text string) {
 	icon := map[string]string{"path": "aws_translate.png"}
-	item := alfredOutputItem{Title: text, Icon: icon}
+	item := alfredOutputItem{Title: text, Arg: text, Icon: icon}
 	output := alfredOutput{Items: []alfredOutputItem{item}}
 	outputJSON, _ := json.Marshal(output)
 	fmt.Print(string(outputJSON))
