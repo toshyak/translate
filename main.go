034369c9e14fa3704df3a33583569eee6172820f
Alexey Osheychik <osheychik@gmail.com>
2020-11-06 19:15:42 +0100

    Get synonyms from Yandex dictionary and show them in response

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"toshyak/translate/aws"
 	"toshyak/translate/spelling"
+	"toshyak/translate/synonyms"
 	"unicode"
 )
 
@@ -25,11 +26,15 @@ func main() {
 		log.Println("Failed to check spelling", err)
 	}
 	translatedText := aws.Translate(textToTranslate, sourceLanguage, translationDirections[sourceLanguage])
+	translatedTextWithSynonyms, err := synonyms.TranslateWithSynonyms(textToTranslate, sourceLanguage, translationDirections[sourceLanguage])
 	out := newOutput()
 	for _, s := range spellingSuggestions {
 		out.add(s, "", "speller", false)
 	}
 	out.add(translatedText, "", "aws", true)
+	for _, s := range translatedTextWithSynonyms {
+		out.add(s, "", "ydict", true)
+	}
 	out.print()
 }
 
